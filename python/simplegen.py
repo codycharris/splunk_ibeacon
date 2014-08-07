c2a88a1e353cda2ecea4ede6753596a3eae3ef86
dagerman <stefansievert@gmail.com>
2014-08-07 10:59:27 -0700

    Updated simplegen.py
    Was generating impossible R1/R2/R3 rssi combinations

--- python/simplegen.py
+++ python/simplegen.py
@@ -3,8 +3,8 @@
 #
 # NOTES:
 # - Events [ab]use the beacon_macaddr field such that it can be used in lieu of a host field 
-# - RRSI values are random with values between 59 and 99
-# - between each loop there is a random sleep interval between 5 and 15 seconds
+# - RRSI values are selected randomly from a set of valid points
+# - between each loop there is a 5-second nap
 #
 #
 
@@ -12,17 +12,22 @@ import sys
 from time import sleep, gmtime, strftime 
 import random
 
+points =  [ [60, 80, 99], 
+            [80, 99, 80],
+            [99, 80, 60],
+            [80, 60, 80],
+            [75, 75, 75],
+            [70, 70, 80],
+            [70, 80, 80]
+        ]
+
 i = 0;
+
+
 events = [
-    '{ "Timestamp":"$ts$", "beacon_macaddr":"R1", "beacon_uuid":"1","beacon_major":"13034","beacon_minor":"2024","tx_pwr_1m":"-59","RSSI":"-$rssi$" }',
-    '{ "Timestamp":"$ts$", "beacon_macaddr":"R2", "beacon_uuid":"1","beacon_major":"13034","beacon_minor":"2024","tx_pwr_1m":"-59","RSSI":"-$rssi$" }',
-    '{ "Timestamp":"$ts$", "beacon_macaddr":"R3", "beacon_uuid":"1","beacon_major":"13034","beacon_minor":"2024","tx_pwr_1m":"-59","RSSI":"-$rssi$" }',
-    '{ "Timestamp":"$ts$", "beacon_macaddr":"R1", "beacon_uuid":"2","beacon_major":"13034","beacon_minor":"2024","tx_pwr_1m":"-59","RSSI":"-$rssi$" }',
-    '{ "Timestamp":"$ts$", "beacon_macaddr":"R2", "beacon_uuid":"2","beacon_major":"13034","beacon_minor":"2024","tx_pwr_1m":"-59","RSSI":"-$rssi$" }',
-    '{ "Timestamp":"$ts$", "beacon_macaddr":"R3", "beacon_uuid":"2","beacon_major":"13034","beacon_minor":"2024","tx_pwr_1m":"-59","RSSI":"-$rssi$" }',
-    '{ "Timestamp":"$ts$", "beacon_macaddr":"R1", "beacon_uuid":"3","beacon_major":"13034","beacon_minor":"2024","tx_pwr_1m":"-59","RSSI":"-$rssi$" }',
-    '{ "Timestamp":"$ts$", "beacon_macaddr":"R2", "beacon_uuid":"3","beacon_major":"13034","beacon_minor":"2024","tx_pwr_1m":"-59","RSSI":"-$rssi$" }',
-    '{ "Timestamp":"$ts$", "beacon_macaddr":"R3", "beacon_uuid":"3","beacon_major":"13034","beacon_minor":"2024","tx_pwr_1m":"-59","RSSI":"-$rssi$" }'
+    '{ "Timestamp":"$ts$", "beacon_macaddr":"R1", "beacon_uuid":"$beacon$","beacon_major":"13034","beacon_minor":"2024","tx_pwr_1m":"-59","RSSI":"-$rssi1$" }',
+    '{ "Timestamp":"$ts$", "beacon_macaddr":"R2", "beacon_uuid":"$beacon$","beacon_major":"13034","beacon_minor":"2024","tx_pwr_1m":"-59","RSSI":"-$rssi2$" }',
+    '{ "Timestamp":"$ts$", "beacon_macaddr":"R3", "beacon_uuid":"$beacon$","beacon_major":"13034","beacon_minor":"2024","tx_pwr_1m":"-59","RSSI":"-$rssi3$" }'
  ]
 
 
@@ -31,12 +36,16 @@ while True:
 #   Generate new timestamp
     ts = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
     i+=1
+#   Generate beaconId 
+    beaconId = str(random.randint(1,3))
+#   Select random point 
+    p = points[random.randint(0,len(points)-1)];
 
     for x in range(0, len(events)):
-        print events[x].replace('$ts$', ts).replace('$rssi$', str(random.randint(59,99)))
+        print events[x].replace('$ts$', ts).replace('$beacon$', beaconId).replace('$rssi1$', str(p[0])).replace('$rssi2$', str(p[1])).replace('$rssi3$', str(p[2]))
         sys.stdout.flush()
 
-    sleep(random.randint(5,15))
+    sleep(5)
     
 #   In case I forget to kill it
     if( i>10000): 
